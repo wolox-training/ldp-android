2bb74989a161fe11e42aaf7bd651fb42e55a0b28
leonplondon <42459619+leonplondon@users.noreply.github.com>
2018-11-23 13:52:37 -0500

    Login UI markup (#1)
    * Renamed application class from bootstrap indications.
    Login UI markup.
    
    * Fixed gradient view properties(width).
    Removed unused code on AndroidManifest.xml

---
+++ app/src/main/java/ar/com/wolox/android/example/ui/login/LoginFragment.java
@@ -0,0 +1,20 @@
+package ar.com.wolox.android.example.ui.login;
+
+import ar.com.wolox.android.R;
+import ar.com.wolox.wolmo.core.fragment.WolmoFragment;
+
+/**
+ * Login fragment;
+ */
+public class LoginFragment extends WolmoFragment {
+
+    @Override
+    public int layout() {
+        return R.layout.fragment_login;
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
