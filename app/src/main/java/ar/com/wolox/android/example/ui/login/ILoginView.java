8a49f30f1b5f51a3c6585e68bbad79b8fd233ba2
León Darío Peña Londoño <leonplondon@gmail.com>
2018-11-26 11:04:10 -0500

    Removed unused view method

--- app/src/main/java/ar/com/wolox/android/example/ui/login/ILoginView.java
+++ app/src/main/java/ar/com/wolox/android/example/ui/login/ILoginView.java
@@ -9,7 +9,6 @@ public interface ILoginView {
     void showEmptyFormErrorOnEmail();
     void showInvalidEmailError();
     void setEmail(@NonNull String email);
-    void setPassword(@NonNull String password);
     void onUserLoggedIn();
     void onWrongEmailOrPassword();
     void showProgress();
