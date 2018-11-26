85159fb2a140f7d0a840b59cf74ee9693d28c669
leonplondon <42459619+leonplondon@users.noreply.github.com>
2018-11-26 13:05:06 -0500

    Login validations (#2)
    * Renamed application class from bootstrap indications.
    Login UI markup.
    
    * Added validations to login form.
    User's credentials are persisted on SharedPreferences.
    
    * Login UI markup (#1)
    
    * Renamed application class from bootstrap indications.
    Login UI markup.
    
    * Fixed gradient view properties(width).
    Removed unused code on AndroidManifest.xml
    
    * Renamed application class from bootstrap indications.
    Login UI markup.
    
    * Added validations to login form.
    User's credentials are persisted on SharedPreferences.
    
    * Applied review requested changes
    
    * Added nullabities annotations
    
    * Fixed typo on annotation
    
    * Fixed PR # 2

--- app/src/main/java/ar/com/wolox/android/example/ui/login/LoginFragment.java
+++ app/src/main/java/ar/com/wolox/android/example/ui/login/LoginFragment.java
@@ -1,12 +1,26 @@
 package ar.com.wolox.android.example.ui.login;
 
+import android.support.annotation.NonNull;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import javax.annotation.Nonnull;
+
 import ar.com.wolox.android.R;
 import ar.com.wolox.wolmo.core.fragment.WolmoFragment;
+import butterknife.BindView;
 
 /**
  * Login fragment;
  */
-public class LoginFragment extends WolmoFragment {
+public class LoginFragment extends WolmoFragment<LoginPresenter> implements ILoginView {
+
+    @BindView(R.id.fragment_login_login_button) Button mLoginButton;
+    @BindView(R.id.fragment_login_sign_up_button) Button mSignUpButton;
+    @BindView(R.id.fragment_login_email_input) EditText mEmailInput;
+    @BindView(R.id.fragment_login_password_input) EditText mPasswordInput;
 
     @Override
     public int layout() {
@@ -14,7 +28,51 @@ public class LoginFragment extends WolmoFragment {
     }
 
     @Override
-    public void init() {
+    public void init() {}
 
+    @Override
+    public void populate() {
+        super.populate();
+        getPresenter().loadEmailAndPassword();
+    }
+
+    @Override
+    public void setListeners() {
+        super.setListeners();
+        mLoginButton.setOnClickListener(view -> onLoginClicked());
+    }
+
+    @Override
+    public void showEmptyFormError() {
+        Toast.makeText(getContext(), R.string.login_empty_form, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void showInvalidEmailError() {
+        mEmailInput.requestFocus();
+        mEmailInput.setError(getString(R.string.login_invalid_email));
+    }
+
+    @Override
+    public void setEmail(@Nonnull String email) {
+        mEmailInput.setText(email);
+    }
+
+    @Override
+    public void setPassword(@NonNull String password) {
+        mPasswordInput.setText(password);
+    }
+
+    void onLoginClicked() {
+        String email = mEmailInput.getText().toString();
+        String password = mPasswordInput.getText().toString();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            showEmptyFormError();
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            showInvalidEmailError();
+        } else {
+            getPresenter().login(email, password);
+        }
     }
 }
---
+++ app/src/main/java/ar/com/wolox/android/example/ui/login/LoginPresenter.java
@@ -0,0 +1,43 @@
+package ar.com.wolox.android.example.ui.login;
+
+import android.support.annotation.NonNull;
+
+import javax.inject.Inject;
+
+import ar.com.wolox.android.example.utils.Constants;
+import ar.com.wolox.wolmo.core.presenter.BasePresenter;
+import ar.com.wolox.wolmo.core.util.SharedPreferencesManager;
+
+/**
+ * The Presenter(P) in the MVP architectural pattern for Login featureclwea
+ */
+public class LoginPresenter extends BasePresenter<ILoginView> {
+
+    private SharedPreferencesManager mSharedPreferencesManager;
+
+    @Inject
+    public LoginPresenter(SharedPreferencesManager mSharedPreferencesManager) {
+        this.mSharedPreferencesManager = mSharedPreferencesManager;
+    }
+
+    /**
+     * Logs in an user;
+     *
+     * @param email    Input email
+     * @param password Input password
+     */
+    void login(@NonNull String email, @NonNull String password) {
+        mSharedPreferencesManager.store(Constants.UserCredentials.USER_EMAIL, email);
+        mSharedPreferencesManager.store(Constants.UserCredentials.USER_PASSWORD, password);
+    }
+
+    /**
+     * Load saved email & password
+     */
+    void loadEmailAndPassword() {
+        runIfViewAttached((view) -> {
+            view.setEmail(mSharedPreferencesManager.get(Constants.UserCredentials.USER_EMAIL, ""));
+            view.setPassword(mSharedPreferencesManager.get(Constants.UserCredentials.USER_PASSWORD, ""));
+        });
+    }
+}
