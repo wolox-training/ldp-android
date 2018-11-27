910fef280dbf674f777c4a3ae1a1eae43fd26a75
León Darío Peña Londoño <leonplondon@gmail.com>
2018-11-27 15:53:12 -0500

    Closed PR#3

--- app/src/main/java/ar/com/wolox/android/example/ui/login/LoginFragment.java
+++ app/src/main/java/ar/com/wolox/android/example/ui/login/LoginFragment.java
@@ -1,7 +1,6 @@
 package ar.com.wolox.android.example.ui.login;
 
 import android.content.Intent;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.text.method.LinkMovementMethod;
 import android.util.Patterns;
