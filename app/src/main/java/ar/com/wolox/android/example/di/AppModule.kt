f2f9c7456d18817647b847730259a3178dabee6c
León Darío Peña Londoño <leonplondon@gmail.com>
2018-11-28 17:35:58 -0500

    Added home tabs

--- app/src/main/java/ar/com/wolox/android/example/di/AppModule.kt
+++ app/src/main/java/ar/com/wolox/android/example/di/AppModule.kt
@@ -3,11 +3,12 @@ package ar.com.wolox.android.example.di
 import ar.com.wolox.android.example.ui.example.ExampleActivity
 import ar.com.wolox.android.example.ui.example.ExampleFragment
 import ar.com.wolox.android.example.ui.home.HomeActivity
+import ar.com.wolox.android.example.ui.home.news.NewsFragment
+import ar.com.wolox.android.example.ui.home.profile.ProfileFragment
 import ar.com.wolox.android.example.ui.login.LoginActivity
 import ar.com.wolox.android.example.ui.login.LoginFragment
 import ar.com.wolox.android.example.ui.signup.SignUpActivity
 import ar.com.wolox.android.example.ui.signup.SignUpFragment
-
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -34,4 +35,10 @@ abstract class AppModule {
 
     @ContributesAndroidInjector
     internal abstract fun mainActivity(): HomeActivity
+
+    @ContributesAndroidInjector
+    internal abstract fun newsFragment(): NewsFragment
+
+    @ContributesAndroidInjector
+    internal abstract fun profileFragment(): ProfileFragment
 }
