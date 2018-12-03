18acf69f7748d824f2a51eaf8cdb88fb5cfc19e1
León Darío Peña Londoño <leonplondon@gmail.com>
2018-12-03 13:17:06 -0500

    Fixed PR#6

--- app/src/main/java/ar/com/wolox/android/example/ui/home/profile/ProfilePresenter.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/home/profile/ProfilePresenter.kt
@@ -3,4 +3,5 @@ package ar.com.wolox.android.example.ui.home.profile
 import ar.com.wolox.wolmo.core.presenter.BasePresenter
 import javax.inject.Inject
 
+// TODO: implement logic
 class ProfilePresenter @Inject constructor() : BasePresenter<IProfileView>()
