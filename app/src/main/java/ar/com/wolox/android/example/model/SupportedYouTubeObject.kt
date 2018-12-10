66f38c8e7b9bb80c9df462c6cf1a717ee7fc05ff
León Darío Peña Londoño <leonplondon@gmail.com>
2018-12-10 15:11:13 -0500

    Removed unused code file

--- app/src/main/java/ar/com/wolox/android/example/model/SupportedYouTubeObject.kt
+++
@@ -1,8 +0,0 @@
-package ar.com.wolox.android.example.model
-
-import java.io.Serializable
-
-/**
- * Model to wrap a [com.google.api.services.youtube.model.SearchResult] to pass ids as extras
- */
-data class SupportedYouTubeObject(val kind: String, val playlistId: String?, val videoId: String?) : Serializable
